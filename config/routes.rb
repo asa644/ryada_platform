867cbc55f245cc76f74fe7dc8077abafeedc35ee
Ahmad Al Msouty <ahmad.msouti@hotmail.com>
2017-11-06 17:15:44 +0200

    initialized certificate

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,9 @@ Rails.application.routes.draw do
   get '/search' => 'listings#search'
   get '/dashboard' => 'users#dashboard'
   get '/profile' => 'users#profile'
-    get '/:locale/*id' => 'pages#show', constraints: {locale: /#{Spina.config.locales.join('|')}/ }
-   get '/:locale/' => 'pages#homepage', constraints: {locale: /#{Spina.config.locales.join('|')}/ }
-   get '/*id' => 'pages#show', as: "page", controller: 'pages', constraints: lambda { |request|
+  get '/:locale/*id' => 'pages#show', constraints: {locale: /#{Spina.config.locales.join('|')}/ }
+  get '/:locale/' => 'pages#homepage', constraints: {locale: /#{Spina.config.locales.join('|')}/ }
+  get '/*id' => 'pages#show', as: "page", controller: 'pages', constraints: lambda { |request|
     !(Rails.env.development? && request.env['PATH_INFO'].starts_with?('/rails/') || request.env['PATH_INFO'].starts_with?("/#{Spina.config.backend_path}") || request.env['PATH_INFO'].starts_with?('/attachments/'))
   }
-
 end
