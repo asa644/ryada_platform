b52f1ceff33385eefc189af2bcfde35a6728ebd2
Ahmad Al Msouty <ahmad.msouti@hotmail.com>
2018-04-19 11:41:32 +0300

    update

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -2,7 +2,7 @@ class BookingMailer < ApplicationMailer
   def confirm(booking)
     @booking = booking  # Instance variable => available in view
     mail(
-      :subject => booking.lesson.listing.name + 'booking',
+      :subject => booking.lesson.listing.name + ' booking',
       :to  => 'hello@localfitt.com',
       :from => 'hello@localfitt.com',
       :track_opens => 'true')
