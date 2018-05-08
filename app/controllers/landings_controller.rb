6e9ca01b91297664f6831e7dbe932c5cd5cc8c10
Ahmad Al Msouty <ahmad.msouti@hotmail.com>
2018-05-08 22:43:14 +0300

    added selection

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -6,22 +6,40 @@ class LandingsController < ApplicationController
     @listings = Listing.all
     @booking = Booking.new
     ordered = Lesson.order(:start_time)
-    # beirut = Category.select do |category|
-    #   category.lessons.listing.city == "Beirut"
-    # end
-    # raise 'h'
-    # beirut = Category.first(3)
-    # jounieh = Category.first(4)
+    beirut = []
+    jounieh = []
+    matn = []
+    Category.all.each do |category|
+      lessons_beirut = Lesson.joins(:listing, :categories)
+      .where('listings.city = ? AND categories.name = ?', "Beirut", "#{category.name}")
+      lessons_jounieh = Lesson.joins(:listing, :categories)
+      .where('listings.city = ? AND categories.name = ?', "Jounieh", "#{category.name}")
+      lessons_matn = Lesson.joins(:listing, :categories)
+      .where('listings.city = ? AND categories.name = ?', "Matn", "#{category.name}")
+      unless lessons_beirut.empty?
+        beirut << category
+      end
+      unless lessons_jounieh.empty?
+        jounieh << category
+      end
+      unless lessons_matn.empty?
+        matn << category
+      end
+    end
     # @beirut= "#{options_from_collection_for_select(beirut, 'id', 'name')}".html_safe
-    # @beirut = "".html_safe
-    # beirut.each do |option|
-    #   @beirut << "<option value=#{option.id}>#{option.name}</option>".html_safe
-    # end
-    # @jounieh = "".html_safe
-    # jounieh.each do |option|
-    #   @jounieh << "<option value=#{option.id}>#{option.name}</option>".html_safe
-    # end
-
+    @beirut = "".html_safe
+    beirut.each do |option|
+      @beirut << "<option value=#{option.id}>#{option.name}</option>".html_safe
+    end
+    @jounieh = "".html_safe
+    jounieh.each do |option|
+      @jounieh << "<option value=#{option.id}>#{option.name}</option>".html_safe
+    end
+    @matn = "".html_safe
+    matn.each do |option|
+      @matn << "<option value=#{option.id}>#{option.name}</option>".html_safe
+    end
+# raise 'e'
     @day = Time.now
     @dayclass = ordered.select do |lesson|
       l = lesson.schedule(Time.now)
