25d1654b6b55d2079b0023121897c5aa32c6f346
Ahmad Al Msouty <ahmad.msouti@hotmail.com>
2018-07-16 13:55:20 +0300

    update

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,6 @@
 class LessonsController < ApplicationController
   before_action :set_lesson, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
 
   # GET /lessons
   # GET /lessons.json
